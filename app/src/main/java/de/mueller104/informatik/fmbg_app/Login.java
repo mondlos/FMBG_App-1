377060d6317e2eb950783d6ebaa41e327bf87fb6
informatik-mueller <informatik.mueller104@gmail.com>
2018-12-17 17:55:11 +0100

    2

--- app/src/main/java/de/mueller104/informatik/fmbg_app/Login.java
+++ app/src/main/java/de/mueller104/informatik/fmbg_app/Login.java
@@ -1,4 +1,17 @@
 package de.mueller104.informatik.fmbg_app;
 
-public class Login {
+import com.google.appinventor.components.runtime.Component;
+import com.google.appinventor.components.runtime.Form;
+import com.google.appinventor.components.runtime.HandlesEventDispatching;
+
+public class Login extends Form implements HandlesEventDispatching {
+    public void $define(){
+
+    }
+
+    @Override
+    public boolean dispatchEvent(Component component, String componentName, String eventName, Object[] args) {
+        return false;
+    }
 }
+
